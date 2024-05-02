389e3699c0dc22834e718d932927b029a6ba53f5
arthur <arthur@marpledata.com>
2024-05-02 14:35:45 -0300

    convert timestamp to Unix int + convert semicircles to degrees

--- app.py
+++ app.py
@@ -17,7 +17,27 @@ def process_fit_file(file):
         records.append(record_data)
 
     df = pd.DataFrame(records)
+
+    df["timestamp"] = pd.to_datetime(df["timestamp"], utc=True).astype("int64") / 10**9
+    df = process_location_columns(df)
+
     return df
+
+
+def process_location_columns(df, columns=None):
+    if columns is None:
+        columns = [c for c in df.columns if c.endswith("_lat") or c.endswith("_long")]
+    else:
+        columns = [c for c in columns if c in df.columns]
+
+    for column in columns:
+        df[column] = semicircles_to_degrees(df[column])
+
+    return df
+
+
+def semicircles_to_degrees(semicircles):
+    return semicircles * (180 / (2**31))
 
 
 def ingest_data():
