d844742d0f291fedb6b2756f5b613bb11edcd69d
arthur <arthur@marpledata.com>
2024-05-02 14:13:51 -0300

    add basic .csv, .fit file parsing to pandas df

--- app.py
+++ app.py
@@ -1,15 +1,41 @@
 import streamlit as st
 import pandas as pd
+import io
+from fitparse import FitFile
+
+
+def upload_to_marple(df):
+    st.write("Uploading data to Marple...")
+
+
+def process_fit_file(file):
+    fitfile = FitFile(file)
+    records = []
+
+    for record in fitfile.get_messages("record"):
+        record_data = {data.name: data.value for data in record}
+        records.append(record_data)
+
+    df = pd.DataFrame(records)
+    return df
 
 
 def ingest_data():
     st.write("Here you can upload your data and quickly get a shareable link with a nice plot of it!")
 
-    file = st.file_uploader("Choose a CSV file", type=["csv"])
+    file = st.file_uploader("Choose a file", type=["csv", "fit"])
 
-    if file and st.button("Ingest"):
-        content = file.read().decode("utf-8")
-        print(content)
+    if file:
+        if file.name.endswith(".csv"):
+            df = pd.read_csv(io.StringIO(file.read().decode("utf-8")))
+        elif file.name.endswith(".fit"):
+            df = process_fit_file(file)
+
+        st.write("Preview of the first 50 rows:")
+        st.dataframe(df.head(50), use_container_width=True)
+
+        if st.button("Upload to Marple"):
+            upload_to_marple(df)
 
 
 if __name__ == "__main__":
