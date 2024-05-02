c4f379cfaecff26a53aa347c4da08fff21bba6bb
arthur <arthur@marpledata.com>
2024-05-02 18:02:29 -0300

    separate share link functionality into multiple functions

--- src/connection.py
+++ src/connection.py
@@ -1,7 +1,6 @@
 import os
 import requests
 import streamlit as st
-
 from marple import Marple
 
 
@@ -9,35 +8,37 @@ class Connection:
     def __init__(self):
         self.token = st.secrets["ACCESS_TOKEN"]
         self.base_url = st.secrets["API_URL"]
-
         self.m = Marple(access_token=self.token, api_url=self.base_url)
         self.m.check_connection()
 
-    def upload_dataframe(self, dataframe, name, marple_folder="/", metadata={}):
-        # upload data
-        file_name = f"{name}.csv"
+    def upload_dataframe(self, dataframe, file_name, marple_folder="/", metadata={}):
         dataframe.to_csv(file_name, sep=",", index=False)
         source_id = self.m.upload_data_file(file_name, marple_folder, metadata=metadata)
         os.remove(file_name)
+        return source_id
 
-        # create sharelink
-        psl_url = ''
+    def create_share_link(self, dataframe, file_name, marple_folder="/", metadata={}, workbook_name="running"):
+        source_id = self.upload_dataframe(dataframe, file_name, marple_folder=marple_folder, metadata=metadata)
 
         endpoint = "/library/share/new"
         auth_header = {"Authorization": f"Bearer {self.token}"}
-        # to-do: make workbook_name a var that changes depending on what's selected in the Streamlit app
-        payload = {"source_ids": [source_id], "workbook_name": "running", "is_public": True}
+        payload = {"source_ids": [source_id], "workbook_name": workbook_name, "is_public": True}
 
         response = requests.post(f"{self.base_url}{endpoint}", headers=auth_header, json=payload)
 
         if response.status_code == 200:
-            # fetch url 
             share_id = response.json().get("message")
-            endpoint = f"/library/share/{share_id}/link"
+            return self.fetch_share_link(share_id)
 
-            response = requests.get(f"{self.base_url}{endpoint}", headers=auth_header, json=payload)
+        return ""
 
-            if response.status_code == 200:
-                psl_url = response.json().get("message")
+    def fetch_share_link(self, share_id):
+        endpoint = f"/library/share/{share_id}/link"
+        auth_header = {"Authorization": f"Bearer {self.token}"}
 
-        return psl_url
+        response = requests.get(f"{self.base_url}{endpoint}", headers=auth_header)
+
+        if response.status_code == 200:
+            return response.json().get("message")
+
+        return ""
