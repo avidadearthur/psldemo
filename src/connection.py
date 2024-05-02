ca1de88129d20755173f5f5acd0250604036dbec
arthur <arthur@marpledata.com>
2024-05-02 16:30:19 -0300

    upload to root as default

--- src/connection.py
+++ src/connection.py
@@ -12,7 +12,7 @@ class Connection:
         self.m = Marple(access_token=self.token, api_url=self.base_url)
         self.m.check_connection()
 
-    def upload_dataframe(self, dataframe, name, marple_folder="/sdk_data", metadata={}):
+    def upload_dataframe(self, dataframe, name, marple_folder="/", metadata={}):
         file_name = f"{name}.csv"
         dataframe.to_csv(file_name, sep=",", index=False)
         source_id = self.m.upload_data_file(file_name, marple_folder, metadata=metadata)
