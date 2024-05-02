212d3259a780770b5dc97d1643ab32ece6f34065
arthur <arthur@marpledata.com>
2024-05-02 17:36:05 -0300

    replace _ by space after process_location_columns(df)

--- src/app.py
+++ src/app.py
@@ -29,6 +29,7 @@ def process_fit_file(file):
     df = df[[col for col in df.columns if not col.startswith("unknown_")]]
     df["timestamp"] = pd.to_datetime(df["timestamp"], utc=True).astype("int64") / 10**9
     df = process_location_columns(df)
+    df.columns = df.columns.str.replace("_", " ", regex=False)
 
     return df
 
