c2651e2fd5ecdd6bfa57a359c62fb089dfc36451
arthur <arthur@marpledata.com>
2024-05-03 00:07:29 -0300

    formatting

--- src/Home.py
+++ src/Home.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+
 def about():
     st.markdown(
         """
@@ -21,8 +22,9 @@ def about():
         
         Arthur
         """,
-        unsafe_allow_html=True
+        unsafe_allow_html=True,
     )
+
 
 if __name__ == "__main__":
     st.set_page_config(page_title="Home", page_icon="🏠", layout="wide")
